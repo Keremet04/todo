2d7c9224fac637b1a88a390720b0543cb5f30ee8
Keremet Dogdurbaeva <dogdurbaeva04@bk.ru>
2022-12-12 21:08:14 +0600

    to do

---
+++ contacts/DetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailViewController.swift
+//  contacts
+//
+//  Created by Керемет  on 29/11/22.
+//
+
+import Foundation
+import UIKit
+//
+//class DetailViewController: UIViewController{
+//    
+//    
+//    @IBOutlet weak var contactLabel:
+//    UILabel!
+//    
+//    
+//    
+//    
+//    
+//    
+//    var name: String = ""
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        contactLabel.text = name 
+//    }
+//}
--- contacts/ViewController.swift
+++ contacts/ViewController.swift
@@ -9,11 +9,106 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var contactsTableView: UITableView!
+
+    var test: [String] = ["clean the house", "throw the trash ", "do homework", "go to gym ", "go to grocery", "wash the car"]
+    
+    
+   
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        contactsTableView.register(UITableViewCell.self, forCellReuseIdentifier: "contact_cell")
+        
+        contactsTableView.dataSource = self
+        contactsTableView.delegate = self
+        
+//
+       navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: self, action: #selector(addTap))
+    }
+
+
+    @objc func addTap(){
+        
+        
+        let
+       // test.append("new task")
+
+     //   test.insert("new task", at: 0)
+       
+        contactsTableView.reloadData()
+    }
+}
+
+
+
+
+
+
+
+
+
+extension ViewController: UITableViewDataSource{
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        
+        return test.count
+        
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+       
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contact_cell", for: indexPath)
+       // cell.textLabel?.text = contacts[indexPath.row]
+        cell.textLabel?.text = test[indexPath.row]
+        cell.imageView?.image = UIImage(systemName: "circle")
+       
+        cell.backgroundColor = .white
+        
+        return cell
     }
 
 
 }
 
+ 
+extension ViewController: UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    
+    //
+    //    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    //        let vc = storyboard?.instantiateViewController(identifier: "detail_vc") as! DetailViewController
+    //
+    
+    
+    //        let person = test[indexPath.row]
+    //        vc.name = person
+    //
+    //
+    //        navigationController?.pushViewController(vc, animated: true)
+    //    }
+    //}
+    //
+    }
